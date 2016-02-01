d759e75f7108ba44852957748de6c8b7f6d4683f
Tom Canham <tom@arcadianproductions.com>
2016-02-01 10:08:33 -0800

    Add views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def status_to_class(status)
+    if (!status || status.site_down == nil)
+      "unknown"
+    elsif status.site_down
+      "down"
+    else
+      "up"
+    end
+  end
+
+  def status_to_text(status)
+    status_to_class(status).upcase
+  end
 end
